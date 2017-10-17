b58c7f19674f8394bb34b3663112124b33d9df4e
john.hasler <john.hasler@LAWN>
2017-10-17 12:25:29 +0100

    Add project files.

---
+++ TakeVideoCameraCrash/TakeVideoCameraCrash/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace TakeVideoCameraCrash
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected void OnTakeVideoButtonClicked(object sender, EventArgs args)
+        {
+            Debug.WriteLine("OnTakeVideoButtonClicked");
+
+            var videoOptions = new StoreVideoOptions
+            {
+                Name = "MyVideo",
+                Directory = "Videos",
+            };
+
+            var file = CrossMedia.Current.TakeVideoAsync(videoOptions);
+
+            Debug.WriteLine("OnTakeVideoButtonClicked_Completed");
+        }
+    }
+}
